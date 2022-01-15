cb47d4b6b62a0e746d7231dcd121dcf60d0d7ae5
jojo3420 <jjjhhhvvv@gmail.com>
2022-01-15 17:26:02 +0900

    정적 자원 사용하기 - public 디렉터리에 자원을 넣는다. - img, css, mp3, mp4 등등 정적자원을 서비스 할 수 있다.

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Index(props) {
   return (
-    <div>Hello nex</div>
+    <div>
+      <h1>스태틱 경로에 정적 자원사용하기</h1>
+      <img src="img/cat.jpeg" alt="cat"/>
+      <hr/>
+      <br/>
+      <audio
+        controls={true}
+        src="mp3/01 H.I.S.Monologue.mp3">
+        your browser does not support the
+        <code>audio</code> element.
+      </audio>
+
+    </div>
   )
 }
 
