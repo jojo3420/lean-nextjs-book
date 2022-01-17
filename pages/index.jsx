d9780701f175d46d5d13d0fadfa4b1a6e44cd631
jojo3420 <jjjhhhvvv@gmail.com>
2022-01-17 22:15:24 +0900

    nextjs with typescript!  - setup typescript and eslint

--- pages/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Index(props) {
-  return (
-    <div>Hello nex</div>
-  )
-}
-
-export default Index
---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const Index: NextPage = (props) => {
+
+  return (
+    <div>
+      <h1>Hello Typescript</h1>
+      <p>타입스크립트 설정한 프로젝트</p>
+      <div>
+      </div>
+    </div>
+  )
+}
+
+
+export default Index
