c030cb7d53ffc455c702efe2568be450db2da2e3
jojo3420 <jjjhhhvvv@gmail.com>
2022-01-20 21:40:27 +0900

    google font 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
@@ -25,6 +25,24 @@ class MyDocument extends Document {
       sheet.seal()
     }
   }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+              <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap" rel="stylesheet"/>
+        </Head>
+        <body>
+        <Main />
+        <NextScript />
+        </body>
+
+      </Html>
+    )
+  }
+
 }
 
 export default MyDocument
--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -9,6 +9,10 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+
+  }
 `
 
 export default GlobalStyle
